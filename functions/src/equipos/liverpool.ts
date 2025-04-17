7173d6fbf046c27c156fb1267540883177d4dfe5
pablo <pablo.emilio.socop@gmail.com>
2025-04-17 14:34:50 +0200

    actualizacion API añadiendo mas jugadores

--- functions/src/equipos/liverpool.ts
+++ functions/src/equipos/liverpool.ts
@@ -3,59 +3,152 @@ import * as functions from "firebase-functions";
 export const getLiverpoolPlayers = functions.https.onRequest(
   (request, response) => {
     const liverpoolPlayers = [
+      // Porteros
       {
         name: "Alisson Becker",
         position: "Goalkeeper",
         overall: 89,
-
       },
+      {
+        name: "Caoimhín Kelleher",
+        position: "Goalkeeper",
+        overall: 78,
+      },
+      {
+        name: "Vítězslav Jaroš",
+        position: "Goalkeeper",
+        overall: 70,
+      },
+      {
+        name: "Harvey Davies",
+        position: "Goalkeeper",
+        overall: 68,
+      },
+
+      // Defensas
       {
         name: "Virgil van Dijk",
         position: "Defender",
         overall: 89,
-
+      },
+      {
+        name: "Ibrahima Konaté",
+        position: "Defender",
+        overall: 83,
+      },
+      {
+        name: "Joe Gomez",
+        position: "Defender",
+        overall: 80,
+      },
+      {
+        name: "Andy Robertson",
+        position: "Defender",
+        overall: 84,
       },
       {
         name: "Trent Alexander-Arnold",
         position: "Defender",
         overall: 87,
-
       },
       {
-        name: "Andy Robertson",
+        name: "Kostas Tsimikas",
         position: "Defender",
-        overall: 86,
-
+        overall: 77,
       },
       {
-        name: "Thiago Alcântara",
-        position: "Midfielder",
-        overall: 86,
+        name: "Jarell Quansah",
+        position: "Defender",
+        overall: 70,
+      },
+      {
+        name: "Conor Bradley",
+        position: "Defender",
+        overall: 72,
+      },
 
+      // Centrocampistas
+      {
+        name: "Wataru Endo",
+        position: "Midfielder",
+        overall: 81,
       },
       {
         name: "Alexis Mac Allister",
         position: "Midfielder",
         overall: 84,
-
       },
+      {
+        name: "Dominik Szoboszlai",
+        position: "Midfielder",
+        overall: 83,
+      },
+      {
+        name: "Curtis Jones",
+        position: "Midfielder",
+        overall: 78,
+      },
+      {
+        name: "Harvey Elliott",
+        position: "Midfielder",
+        overall: 77,
+      },
+      {
+        name: "Ryan Gravenberch",
+        position: "Midfielder",
+        overall: 80,
+      },
+      {
+        name: "James McConnell",
+        position: "Midfielder",
+        overall: 68,
+      },
+      {
+        name: "Trey Nyoni",
+        position: "Midfielder",
+        overall: 65,
+      },
+
+      // Delanteros
       {
         name: "Mohamed Salah",
         position: "Forward",
         overall: 90,
-
-      },
-      {
-        name: "Darwin Núñez",
-        position: "Forward",
-        overall: 84,
-
       },
       {
         name: "Luis Díaz",
         position: "Forward",
         overall: 85,
-
+      },
+      {
+        name: "Darwin Núñez",
+        position: "Forward",
+        overall: 84,
+      },
+      {
+        name: "Diogo Jota",
+        position: "Forward",
+        overall: 83,
+      },
+      {
+        name: "Cody Gakpo",
+        position: "Forward",
+        overall: 82,
+      },
+      {
+        name: "Federico Chiesa",
+        position: "Forward",
+        overall: 85,
+      },
+      {
+        name: "Ben Doak",
+        position: "Forward",
+        overall: 70,
+      },
+      {
+        name: "Trent Koné-Doherty",
+        position: "Forward",
+        overall: 68,
       },
     ];
 
--- functions/src/equipos/manCity.ts
+++ functions/src/equipos/manCity.ts
@@ -3,24 +3,71 @@ import * as functions from "firebase-functions";
 export const getManCityPlayers = functions.https.onRequest(
   (request, response) => {
     const manCityPlayers = [
+      // Porteros
       {
         name: "Ederson",
         position: "Goalkeeper",
         overall: 88,
+      },
+      {
+        name: "Stefan Ortega",
+        position: "Goalkeeper",
+        overall: 80,
+      },
+      {
+        name: "Scott Carson",
+        position: "Goalkeeper",
+        overall: 70,
+      },
 
+      // Defensores
+      {
+        name: "Kyle Walker",
+        position: "Defender",
+        overall: 84,
       },
       {
         name: "Rúben Dias",
         position: "Defender",
         overall: 88,
-
       },
       {
         name: "John Stones",
         position: "Defender",
         overall: 85,
-
       },
+      {
+        name: "Nathan Aké",
+        position: "Defender",
+        overall: 83,
+      },
+      {
+        name: "Joško Gvardiol",
+        position: "Defender",
+        overall: 84,
+      },
+      {
+        name: "Manuel Akanji",
+        position: "Defender",
+        overall: 82,
+      },
+      {
+        name: "Rico Lewis",
+        position: "Defender",
+        overall: 75,
+      },
+      {
+        name: "Vitor Reis",
+        position: "Defender",
+        overall: 70,
+      },
+      {
+        name: "Abdukodir Khusanov",
+        position: "Defender",
+        overall: 72,
+      },
+
+      // Centrocampistas
       {
         name: "Rodri",
         position: "Midfielder",
@@ -30,31 +77,83 @@ export const getManCityPlayers = functions.https.onRequest(
         name: "Kevin De Bruyne",
         position: "Midfielder",
         overall: 90,
-
       },
       {
         name: "Bernardo Silva",
         position: "Midfielder",
         overall: 88,
-
       },
+      {
+        name: "Mateo Kovačić",
+        position: "Midfielder",
+        overall: 84,
+      },
+      {
+        name: "İlkay Gündoğan",
+        position: "Midfielder",
+        overall: 85,
+      },
+      {
+        name: "Matheus Nunes",
+        position: "Midfielder",
+        overall: 82,
+      },
+      {
+        name: "Nico González",
+        position: "Midfielder",
+        overall: 80,
+      },
+      {
+        name: "James McAtee",
+        position: "Midfielder",
+        overall: 75,
+      },
+      {
+        name: "Nico O'Reilly",
+        position: "Midfielder",
+        overall: 70,
+      },
+
+      // Delanteros
       {
         name: "Erling Haaland",
         position: "Forward",
         overall: 91,
-
       },
       {
         name: "Phil Foden",
         position: "Forward",
         overall: 88,
-
       },
       {
         name: "Jack Grealish",
         position: "Forward",
         overall: 84,
-
+      },
+      {
+        name: "Jérémy Doku",
+        position: "Forward",
+        overall: 83,
+      },
+      {
+        name: "Savinho",
+        position: "Forward",
+        overall: 78,
+      },
+      {
+        name: "Oscar Bobb",
+        position: "Forward",
+        overall: 75,
+      },
+      {
+        name: "Omar Marmoush",
+        position: "Forward",
+        overall: 80,
+      },
+      {
+        name: "Claudio Echeverri",
+        position: "Forward",
+        overall: 72,
       },
     ];
 
